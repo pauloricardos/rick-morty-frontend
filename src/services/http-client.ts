8ca6c255831499d3df37e75089aeaf68d4c7c01e
Paulo Ricardo <paulorsscontato@hotmail.com>
2024-07-14 23:55:03 -0300

    feat: add env variable

--- src/services/http-client.ts
+++ src/services/http-client.ts
@@ -1,5 +1,5 @@
 import { GraphQLClient } from 'graphql-request';
 
-const API_URL = 'https://rickandmortyapi.com/graphql';
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const client = new GraphQLClient(API_URL);
